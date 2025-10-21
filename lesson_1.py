1a5dbc6c2798af74f6b8c8ff54e60a1a619d24d5
Bart Dorsey <bart@bartdorsey.com>
2025-10-21 09:58:08 -0500

    Fixing flake8 problems in starter comments

--- lesson_1.py
+++ lesson_1.py
@@ -1,23 +1,26 @@
 # Variables and Data Types Lab
-## Enter your shell commands under each question heading:
-##1. A variable that holds your first name.
-##<!-- Your code here -->
+# Enter your shell commands under each question heading:
+# 1. A variable that holds your first name.
+# <!-- Your code here -->
 
-##2. A variable that holds your last name.
-##<!-- Your code here -->
+# 2. A variable that holds your last name.
+# <!-- Your code here -->
 
-##3. A variable that holds your full name without using a new string.
-##<!-- Your code here -->
+# 3. A variable that holds your full name without using a new string.
+# <!-- Your code here -->
 
-##5. A boolean variable called isCertified that is set to true.
-##<!-- Your code here -->
+# 5. A boolean variable called isCertified that is set to true.
+# <!-- Your code here -->
 
-##6. A variable that holds the number of miles to the moon.
-##<!-- Your code here -->
+# 6. A variable that holds the number of miles to the moon.
+# <!-- Your code here -->
 
-##7. A variable that holds the uses your previous answer to calculate the number of ft to the moon.
-##<!-- Your code here -->
+# 7. A variable that holds the uses your previous answer to calculate the
+# number of ft to the moon.
+# <!-- Your code here -->
 
-##8. A print statement that uses the the previous variables to print the following. 
-##"Welcome cadet {fullname}. Today you will be traveling {ft_to_moon} ft into space! Are you space-worthy? {isCertified}"
-##<!-- Your code here -->
+# 8. A print statement that uses the the previous variables to print the
+# following.
+# "Welcome cadet {fullname}. Today you will be traveling {ft_to_moon} ft into
+# space! Are you space-worthy? {isCertified}"
+# <!-- Your code here -->
