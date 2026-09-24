4920e38e908c5d1e6b2808cbb5096bdcc8587fef
emiliojroybal <emiliojroybal@gmail.com>
2026-09-24 13:30:06 -0700

    Finished project

--- lesson_1.py
+++ lesson_1.py
@@ -1,26 +1,26 @@
 # Variables and Data Types Lab
 # Enter your shell commands under each question heading:
 # 1. A variable that holds your first name.
-# <!-- Your code here -->
+first_name = "Emilio"
 
 # 2. A variable that holds your last name.
-# <!-- Your code here -->
+last_name = "Roybal"
 
 # 3. A variable that holds your full name without using a new string.
-# <!-- Your code here -->
+full_name = first_name + " " + last_name
 
 # 5. A boolean variable called isCertified that is set to true.
-# <!-- Your code here -->
+isCertified = True
 
 # 6. A variable that holds the number of miles to the moon.
-# <!-- Your code here -->
+miles_to_moon = 238855
 
 # 7. A variable that holds the uses your previous answer to calculate the
 # number of ft to the moon.
-# <!-- Your code here -->
+ft_to_moon = miles_to_moon * 5280
 
 # 8. A print statement that uses the the previous variables to print the
 # following.
 # "Welcome cadet {fullname}. Today you will be traveling {ft_to_moon} ft into
 # space! Are you space-worthy? {isCertified}"
-# <!-- Your code here -->
+print(f"Welcome cadet {full_name}. Today you will be traveling {ft_to_moon} ft into space! Are you space-worthy? {isCertified}")
